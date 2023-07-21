e2da0cb30732644a952bd58cb4a3404396c493e9
Sisi Small <smallsianah@gmail.com>
2023-07-20 21:59:27 -0500

    added features to fix storing local storage and alignment of text and proper formatting of text

--- src/App.js
+++ src/App.js
@@ -1,85 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [website, setWebsite] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  
-  const loadFromLocalStorage = () => {
-    const savedEntries = localStorage.getItem('entries');
-    return savedEntries ? JSON.parse(savedEntries) : [];
-  };
+  // State declarations
+  const [website, setWebsite] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [entries, setEntries] = useState([]);
 
-  const initialEntries = loadFromLocalStorage();
-  const [entries, setEntries] = useState(initialEntries);
-
-  const saveToLocalStorage = (newEntries) => {
-    localStorage.setItem('entries', JSON.stringify(newEntries));
-  };
+  // Check local storage on component mount and update the state if needed
+  useEffect(() => {
+    const storedEntries = localStorage.getItem('passwordEntries');
+    if (storedEntries) {
+      setEntries(JSON.parse(storedEntries));
+    }
+  }, []);
 
   const addEntry = () => {
-    if (website && username && password) { // Ensure fields aren't empty
-      const newEntry = `${website} | ${username} | ${password}`;
-      const updatedEntries = [...entries, newEntry];
-      setEntries(updatedEntries);
-      saveToLocalStorage(updatedEntries);
+    if (website && username && password) {
+      const newEntry = { website, username, password };
+      const newEntries = [...entries, newEntry];
+      setEntries(newEntries);
 
-      // Clear the input fields
-      setWebsite("");
-      setUsername("");
-      setPassword("");
+      // Update local storage
+      localStorage.setItem('passwordEntries', JSON.stringify(newEntries));
+
+      // Clear the form fields
+      setWebsite('');
+      setUsername('');
+      setPassword('');
     }
   };
 
-  const handleDelete = (indexToDelete) => {
-    const updatedEntries = entries.filter((_, index) => index !== indexToDelete);
+  const handleDelete = (index) => {
+    const updatedEntries = [...entries];
+    updatedEntries.splice(index, 1);
     setEntries(updatedEntries);
-    saveToLocalStorage(updatedEntries);
+
+    // Update local storage
+    localStorage.setItem('passwordEntries', JSON.stringify(updatedEntries));
   };
 
   return (
     <div className="App">
-      <h2>Password Manager</h2>
-  
-      <div style={{ display: 'flex', gap: '10px', marginBottom: '20px' }}>
+      <h2>Code Keeper</h2>
+      <h2>Leave those password guessing games behind</h2>
+      <div style={{ display: 'inline-flex', gap: '10px', marginBottom: '20px' }}>
         <input
+          type="text"
+          placeholder="Website"
           value={website}
           onChange={(e) => setWebsite(e.target.value)}
-          placeholder="Website"
-          style={{ border: '2px solid #ddd', padding: '5px', borderRadius: '15px' }}
         />
         <input
+          type="text"
+          placeholder="Username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-          style={{ border: '2px solid #ddd', padding: '5px', borderRadius: '15px' }}
         />
         <input
+          type="password"
+          placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          style={{ border: '2px solid #ddd', padding: '5px', borderRadius: '15px' }}
         />
-        <button onClick={addEntry} style={{ borderRadius: '15px' }}>
-          Add
-        </button>
+        <button onClick={addEntry}>Add Entry</button>
       </div>
-  
       <ul>
         {entries.map((entry, index) => (
           <li key={index}>
-            {entry} 
-            <button onClick={() => handleDelete(index)} style={{ marginLeft: '10px' }}>Delete</button>
+            <strong>Website:</strong> {entry.website} | 
+            <strong>Username:</strong> {entry.username} | 
+            <strong>Password:</strong> {entry.password} 
+            <button onClick={() => handleDelete(index)} style={{backgroundColor: 'red', color: 'white', marginLeft: '10px'}}>Delete</button>
+
+
           </li>
         ))}
       </ul>
     </div>
   );
-  
 }
 
 export default App;
+
+
+
+
 
 
 
